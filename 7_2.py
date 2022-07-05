d5a4e13685f58d3c63130720627b50c9b5bd565e
Gamz148 <97736124+Gamz148@users.noreply.github.com>
2022-07-05 23:40:13 +0200

    Update 7_2.py

--- 7_2.py
+++ 7_2.py
@@ -1,6 +1,7 @@
 import os
 
 
+
 def parse_yaml_string(src_string, space = 2):
 
     lst_yaml_string = src_string.split("-")
@@ -12,14 +13,15 @@ def parse_yaml_string(src_string, space = 2):
     else:
         isfile = True
     lenght = len(lst_yaml_string)
+
     if lenght > 1:
         level = len(lst_yaml_string[0]) // space
     else:
         level = 0
     return level, name, isfile
 
-# prefix_name = "task_2"
-yaml_path = os.path.join(".", "config_2.yaml")
+prefix_name = "task_7_2"
+yaml_path = os.path.join(".", prefix_name, "config_2.yaml")
 current_path = []
 current_level = -1
 with open(yaml_path, encoding="UTF-8", mode = "rt") as file:
@@ -27,21 +29,21 @@ with open(yaml_path, encoding="UTF-8", mode = "rt") as file:
         level, name, isfile = parse_yaml_string(line)
 
         if current_level < level:
-            # Увеличение уровня вложенности
+
             current_path.append(name)
             current_level += 1
         elif current_level == level:
-            # Тот  же уровень вложенности, но объект другой
+
             current_path[-1] = name
         else:
-            # Уменьшение уровня вложенности (не обязательно на 1)
+
             for i in range(current_level-level+1):
                 current_path.pop()
                 current_level -= 1
             current_level += 1
             current_path.append(name)
 
-        dir_path = os.path.join(".",*current_path)
+        dir_path =  os.path.join(".",prefix_name,*current_path)
         if not os.path.exists(dir_path):
             if isfile:
                 # Этот код для windows, Unix системы можно исполозвать mknod
