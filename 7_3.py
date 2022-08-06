b1db6c0d89f05611e2229a1c88807d3f83a2d9bd
Gamz <gamz_1@mail.ru>
2022-08-06 21:15:57 +0200

    new 7_3

--- 7_3.py
+++ 7_3.py
@@ -1,13 +1,45 @@
-from os import listdir, walk
-from os.path import join
-from shutil import copytree
+
+import os
+from os import mkdir
+from os.path import join, exists
+import shutil
 
 
+folder = (r'C:\Users\Олег\PycharmProjects\DZ_7_1')
+print(os.getcwd()) # проверка месоположения
 
-root_dir = join(".", "task_3")
-base_prj_dir = listdir(root_dir)[0]  # Считаем, что в корне - одна директория
-templates_dir = join(root_dir, base_prj_dir, "templates")
-for root, dirs, files in walk(root_dir):
-    print(root, dirs, files)
-    if "templates" in dirs:
-        copytree(join(root,"templates"), templates_dir, dirs_exist_ok = True)
+#создать иерархию папок
+dir_path = r'task_3\my_project'
+if not os.path.exists(dir_path):
+    os.makedirs(dir_path)
+
+first_dir = 'task_3'
+main_dir = 'my_project'
+main_dir1 = 'templates'
+sub_dirs = ["mainapp","authapp"]
+
+main_path = join(".",first_dir, main_dir,"..",main_dir1)
+if not exists(main_path): mkdir(main_path)
+
+for sub_dir in sub_dirs:
+    sub_path = join(".",first_dir,main_dir,"..",main_dir1, sub_dir)
+    if not exists(sub_path): mkdir(sub_path)
+
+#добавление файлов html
+file1 = open('base.html', 'wt')
+print('Oops, I created a file.', file=file1)
+file1.close()
+file2 = open('index.html', 'wt')
+print('created a file2.', file=file2)
+file2.close()
+
+file1 = open('base.html', 'wt')
+print('Oops, I created a file.', file=file1)
+file1.close()
+file2 = open('index.html', 'wt')
+print('created a file2.', file=file2)
+file2.close()
+
+# перемещение файлов
+shutil.move('base.html', 'task_3/templates/authapp')
+shutil.move('index.html', 'task_3/templates/authapp')
